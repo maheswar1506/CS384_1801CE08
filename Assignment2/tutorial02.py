886668e3b262597b2e603354ad732a330a4ce3b9
maheswar1506 <maheswarreddy1506@gmail.com>
2020-09-23 22:56:28 +0530

    mse function is done in Assign 2

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -44,7 +44,22 @@ def rmse(first_list, second_list):
 # Function to compute mse. You cant use Python functions
 def mse(first_list, second_list):
     # mse Logic
+    summation_value = 0
+    if len(first_list) == len(second_list):
+        for i in range(len(first_list)):
+            if isinstance(first_list[i],(int,float)) and isinstance(second_list[i],(int,float)):
+                summation_value = summation_value + ((first_list[i]-second_list[i])**2)
+            else:
+                return 0
+    else:
+        return 0
+    mse_value = 0
+    if len(first_list)>0:
+        mse_value = summation_value/len(first_list)
+    else:
+        return 0
     return mse_value
+
 
 
 # Function to compute mae. You cant use Python functions
