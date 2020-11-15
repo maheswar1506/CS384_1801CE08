a51d88b8901bd727ffd2824eda6b285969a2969f
maheswar1506 <maheswarreddy1506@gmail.com>
2020-11-15 20:24:45 +0530

    Files in How I Met Your Mother folder are renamed by shadow master

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -115,7 +115,40 @@ def rename_Suits(folder_name):
         pass
 
 def rename_How_I_Met_Your_Mother(folder_name):
-    pass
+    try:
+        season_pad = int(input())
+        episode_pad = int(input())
+        basepath = "./Subtitles"
+        folder_path = os.path.join(basepath, folder_name)
+        files = os.listdir(folder_path)
+        for file in files:
+            split_file = file.split('-')
+            series_name = split_file[0]
+            middle_split = split_file[1].split('x')
+            middle_split = [ x.strip() for x in middle_split]
+            season_num = middle_split[0]
+            season_num = str(int(season_num))
+            if len(split_file) > 3 and split_file[2].strip().isdigit():
+                episode_num = str(int(middle_split[1])).zfill(episode_pad) + "-" + str(int(split_file[2].strip())).zfill(episode_pad)
+                end_split = split_file[3].split('.')
+                episode_name = end_split[0]
+                episode_name = episode_name.strip()
+                extension = file.split('.')[-1]
+                newname = series_name + "- " + "Season " + season_num.zfill(season_pad) + " Episode " + episode_num + " - " + episode_name + "." + extension
+                os.rename(os.path.join(folder_path, file), os.path.join(folder_path,newname))   
+
+            else:
+                episode_num = middle_split[1]
+                episode_num = str(int(episode_num))
+                end_split = split_file[2].split('.')
+                episode_name = end_split[0]
+                episode_name = episode_name.strip()
+                extension = file.split('.')[-1]
+
+                newname = series_name + "- " + "Season " + season_num.zfill(season_pad) + " Episode " + episode_num.zfill(episode_pad) + " - " + episode_name + "." + extension
+                os.rename(os.path.join(folder_path, file), os.path.join(folder_path,newname))
+    except:
+        pass
 
     
 #folder_inp = input()
@@ -129,5 +162,8 @@ def rename_How_I_Met_Your_Mother(folder_name):
 #folder_inp = "Sherlock"
 #rename_Sherlock(folder_inp)
 
-folder_inp = "Suits"
-rename_Suits(folder_inp)
+#folder_inp = "Suits"
+#rename_Suits(folder_inp)
+
+folder_inp = "How I Met Your Mother"
+rename_How_I_Met_Your_Mother(folder_inp)
