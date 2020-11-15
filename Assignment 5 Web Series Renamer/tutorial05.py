a5c99d028f8b21d835fd1943c4db33e24d0e47ea
maheswar1506 <maheswarreddy1506@gmail.com>
2020-11-15 20:20:28 +0530

    Files in Sherlock folder are renamed by shadow master

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -57,7 +57,25 @@ def rename_Game_of_Thrones(folder_name):
 
 
 def rename_Sherlock(folder_name):
-    pass
+    try:
+        season_pad = int(input())
+        episode_pad = int(input())
+        basepath = "./Subtitles"
+        folder_path = os.path.join(basepath, folder_name)
+        files = os.listdir(folder_path)
+        for file in files:
+            split_file = file.split('.')
+            series_name = split_file[0]
+            extension = split_file[-1]
+            list_of_all_numbers_in_filename = re.findall('[0-9]+',file)
+            season_num = list_of_all_numbers_in_filename[0]
+            season_num = str(int(season_num))
+            episode_num = list_of_all_numbers_in_filename[1]
+            episode_num = str(int(episode_num))
+            newname = series_name + " - Season " + season_num.zfill(episode_pad) + " - " + "Episode " + episode_num.zfill(episode_pad) + "." + extension
+            os.rename(os.path.join(folder_path, file), os.path.join(folder_path,newname))
+    except:
+        pass
 
 
 def rename_Suits(folder_name):
@@ -72,5 +90,8 @@ def rename_How_I_Met_Your_Mother(folder_name):
 #folder_inp = "FIR"
 #rename_FIR(folder_inp)
 
-folder_inp = "Game of Thrones"
-rename_Game_of_Thrones(folder_inp)
+#folder_inp = "Game of Thrones"
+#rename_Game_of_Thrones(folder_inp)
+
+folder_inp = "Sherlock"
+rename_Sherlock(folder_inp)
