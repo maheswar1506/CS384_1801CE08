5d4c2b47d38017a0a824325d5b530eba81e3fb1f
maheswar1506 <maheswarreddy1506@gmail.com>
2020-12-03 01:49:55 +0530

    Restructuring the folders without removing input data by shadow master

---
+++ Assignments/Assignment_5/test_cases_tutorial05.py
@@ -0,0 +1,16 @@
+import tutorial05 A5
+
+# folder_inp = "FIR"
+# A5.rename_FIR(folder_inp)
+
+# folder_inp = "Game of Thrones"
+# A5.rename_Game_of_Thrones(folder_inp)
+
+# folder_inp = "Sherlock"
+# A5.rename_Sherlock(folder_inp)
+
+# folder_inp = "Suits"
+# A5.rename_Suits(folder_inp)
+
+# folder_inp = "How I Met Your Mother"
+# A5.rename_How_I_Met_Your_Mother(folder_inp)
